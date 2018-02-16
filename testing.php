30d40e3db31e7c566c217b62f95148efc59d2308
Andrew Muliar <andrewmuliar7@gmail.com>
2018-02-16 18:20:05 +0200

    upgrd

--- testing.php
+++ testing.php
@@ -8,6 +8,7 @@ function HashRequest($data)
   {
    $new_data[] = redo($data[$i]);
   }
+ print_r($new_data);
  return $new_data;
 }
 
@@ -17,53 +18,66 @@ function HashRequest($data)
   $ar = array();
   foreach($arrayka as $key => $value)
   {
-   //ho $arrayka->$key;
+   //If key has another level childs, he go recurse himself
    if(gettype($arrayka->$key) == 'object')
    {
-  //  echo ' object'.PHP_EOL;
-	$ar[$key] = redo($arrayka->$key);
+	$ar[$key] = redo($arrayka->$key); //recurse
    }
-   else
+   else // if key simple key no multydimension level
      $ar[$key] = hash('sha256',$value);
-  // else echo 'not object'.PHP_EOL;
   }
   return $ar;
  }
- /*for($i = 0; $i < count($array_of_object); $i++)
- {
-  //$new_data[key($array_of_object[$i])] = $array[$i]['position'];
-  $key = key($array_of_object[$i]);
-  $value = $array_of_object[$i]->$key;
-  if(array_key_exists($value, $array_of_object))
-  {
-   echo 'exits';
-   echo $array_of_object[$i]->$value;
-  }
-  echo ' type = '.gettype($array_of_object[$i]).' '.key($array_of_object[$i]);
-  $new_data = array_values($array_of_object);
- }*/
 
-/*try
-{*/
- $request = 'Request type: '.$_SERVER['REQUEST_METHOD'];
- $post = file_get_contents('php://input',true);
- $json = json_decode($post);
- $filename = 'super_log_file.txt';
- $hasfile = 'super_hash_data.txt';
- $today = getdate();
- $type = " Data type: ".gettype($post);
- $date = 'Date: '.$today['hours'].':'.$today['wday'].':'.$today['minutes'].' '.$today['month'].' '.$today['wday'];
- $line = '====================================================';
- print_r(HashRequest($json));
- $hashdata = json_encode(HashRequest($json));
- $log = $line.PHP_EOL.$request.PHP_EOL.$type.PHP_EOL.' '.$post.' '.PHP_EOL.$date.PHP_EOL;
- $haslog = $line.PHP_EOL.$request.PHP_EOL.$type.PHP_EOL.' '.$hashdata.' '.PHP_EOL.$date.PHP_EOL;
- file_put_contents($filename, $log, FILE_APPEND);
- file_put_contents($hasfile, $haslog, FILE_APPEND);
+ /*creating API request to BX8, hashing and put in logs files*/
+ function takeBX($module)
+ {
+  $url = 'http://affiliates.bx8.me/?MODULE='.$module.'&COMMAND=View&api_username=RND@leomarkets.com&api_password=2Aj484$!2A';
+  $ch = curl_init($url);
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,true); //LEADS
+  curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,20);
+  curl_setopt($ch,CURLOPT_TIMEOUT,30);
+  curl_setopt($ch,CURLOPT_FOLLOWLOCATION, false);
+  curl_setopt($ch,CURLOPT_HTTPHEADER,["Content-Type:application/x-www-form-urlencoded; charset=utf-8"]);
+  curl_setopt($ch,CURLOPT_HEADER, false);
+  $exec = curl_exec($ch);
+  $response = json_decode($exec);
+  if($response->status == 'OK')
+  {
+   $request = 'Request type: '.$_SERVER['REQUEST_METHOD'];
+   $filename = 'super_log_file.txt';
+   $hasfile = 'super_hash_data.txt';
+   $today = getdate();
+   $date = 'Date: '.$today['hours'].':'.$today['wday'].':'.$today['minutes'].' '.$today['month'].' '.$today['wday'];
+   $line = '===================================================================';
+   $hashdata = json_encode(HashRequest($response->customers));
+   $post = $response->customers;
+   $post = json_encode($post);
+   $type = " Data type: ".gettype($post);
+   $log = $line.PHP_EOL.$request.PHP_EOL.$type.PHP_EOL.' '.$post.' '.PHP_EOL.$date.PHP_EOL;
+   $haslog = $line.PHP_EOL.$request.PHP_EOL.$type.PHP_EOL.' '.$hashdata.' '.PHP_EOL.$date.PHP_EOL;
+   file_put_contents($filename, $log, FILE_APPEND);
+   file_put_contents($hasfile, $haslog, FILE_APPEND);
+  }
+  else
+  echo 'error';
+ }
+
+
+ takeBX('Customer');
+  /*  $request = 'Request type: '.$_SERVER['REQUEST_METHOD'];
+   $post = file_get_contents('php://input',true);
+   $json = json_decode($post);
+   $filename = 'super_log_file.txt';
+   $hasfile = 'super_hash_data.txt';
+   $today = getdate();
+   $type = " Data type: ".gettype($post);
+   $date = 'Date: '.$today['hours'].':'.$today['wday'].':'.$today['minutes'].' '.$today['month'].' '.$today['wday'];
+   $line = '====================================================';
+   $hashdata = HashRequest($response->customers);
+   $log = $line.PHP_EOL.$request.PHP_EOL.$type.PHP_EOL.' '.$post.' '.PHP_EOL.$date.PHP_EOL;
+   $haslog = $line.PHP_EOL.$request.PHP_EOL.$type.PHP_EOL.' '.$hashdata.' '.PHP_EOL.$date.PHP_EOL;
+   file_put_contents($filename, $log, FILE_APPEND);
+   file_put_contents($hasfile, $haslog, FILE_APPEND);*/
  echo ' Status = Ok';
-/*}
-catch (Exception $e)
-{
- echo 'Error = '.$e->getMessage(),'\n';
-}*/
 ?>
