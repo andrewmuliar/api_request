b2753511cf361b1ace685de66e472d4d41720c93
Andrew Muliar <andrewmuliar7@gmail.com>
2018-02-28 16:36:07 +0200

    upgrd

--- fullrequest.php
+++ fullrequest.php
@@ -42,13 +42,19 @@ function backToString($array)
  /*creating API request to get data from BX8, hashing and put in logs files*/
  function takeData()
  {
+// Request options
   $module = 'Lead';
   $api_username = 'RND@leomarkets.com';
   $api_password = '2Aj484$!2A';
   $recordsToShow = 15; //MAX 500 records
   $recordStart = 0; //PAGES by 500 records START FROM 0
-  $url = 'http://affiliates.bx8.me/?MODULE='.$module.'&COMMAND=View&LIMIT[recordsToShow]='.$recordsToShow.'&LIMIT[recordStart]='.$recordStart.'&api_username='.$api_username.'&api_password='.$api_password;
-  //MODULE=Customer&COMMAND=view&LIMIT[recordsToShow]=5&LIMIT[recordStart]=0
+  $url = 'http://affiliates.bx8.me/?MODULE='.$module;
+  $url .= '&COMMAND=View';
+  $url .= '&LIMIT[recordsToShow]='.$recordsToShow;
+  $url .= '&LIMIT[recordStart]='.$recordStart;
+  $url .= '&api_username='.$api_username;
+  $url .= '&api_password='.$api_password;
+//Init curl
   $ch = curl_init($url);
   curl_setopt($ch,CURLOPT_RETURNTRANSFER,true); //LEADS
   curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,20);
@@ -59,7 +65,7 @@ function backToString($array)
   $exec = curl_exec($ch);
   $response = json_decode($exec);
   print_r($response);
-  if($response->status == 'OK')
+  if($response->status == 'OK') //Ok
   {
    $filename = 'super_log_file.txt';
    $hashfile = 'super_hash_data.txt';
@@ -75,28 +81,12 @@ function backToString($array)
    file_put_contents($hashfile, $hashlog, FILE_APPEND);
   }
   else
-   echo 'error';
+   echo 'Error = '.$response->status;
 
-  curl_close($ch);
+  curl_close($ch); //close connection 
  }
 
+//Init all functions to get Bx8 data, log files, API facebook integration
  takeData();
- /*creating log files*/
- /* $request = 'Request type: '.$_SERVER['REQUEST_METHOD'];
-  $post = file_get_contents('php://input',true);
-  $json = json_decode($post);
-  $filename = 'super_log_file.txt';
-  $hasfile = 'super_hash_data.txt';
-  $today = getdate();
-  $type = "Data type: ".gettype($post);
-  $date = 'Date: '.$today['hours'].':'.$today['wday'].':'.$today['minutes'].' '.$today['month'].' '.$today['wday'];
-  $line = '==== New transaction ======';
-  $hashdata = HashRequest($json);
-  $hashdata = backToString($hashdata);
-  echo $hashdata;
-  $log = $line.PHP_EOL.$type.PHP_EOL.' '.$request.' '.PHP_EOL.$post.PHP_EOL.$date.PHP_EOL;
-  $haslog = $line.PHP_EOL.$request.PHP_EOL.' '.$hashdata.' '.PHP_EOL.$date.PHP_EOL;
-  file_put_contents($filename, $log, FILE_APPEND);
-  file_put_contents($hasfile, $haslog, FILE_APPEND);
-  echo ' Status = Ok';*/
+
 ?>
